c06df310932aa3d99d06c3a765b1b81cac3ca708
Keido Sumi <delta0726@nta.pial.jp>
2022-04-20 06:55:34 +0900

    Update chap2-6

--- book/bayesian_model/2-6_Stanコーディングの詳細.R
+++ book/bayesian_model/2-6_Stanコーディングの詳細.R
@@ -1,15 +1,15 @@
-#***************************************************************************************
-# Title     : ベイズ統計モデリングによるデータ分析入門
-# Chapter   : 2-6 Stanコーディングの詳細
-# Objective : TODO
-# Created by: Owner
-# Created on: 2021/2/10
-# Page      : P141 - P150
-#***************************************************************************************
+# **********************************************************************************
+# Title   : ベイズ統計モデリングによるデータ分析入門
+# Chapter : 2 RとStanによるデータ分析
+# Theme   : 6 Stanコーディングの詳細
+# Date    : 2022/4/18
+# Page    : P141 - P150
+# URL     : https://logics-of-blue.com/r-stan-bayesian-model-intro-book-support/
+# **********************************************************************************
 
 
-# ＜テーマ＞
-# - Stanのコーディング方法
+# ＜概要＞
+# - Stanのコーディング方法や文法について確認する
 
 
 # ＜目次＞
@@ -33,10 +33,9 @@ library(bayesplot)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-
 # データロード
-file_beer_sales_1 <- read_csv("book/bayesian_model/csv/2-4-1-beer-sales-1.csv")
-file_beer_sales_ab <- read_csv("book/bayesian_model/csv/2-6-1-beer-sales-ab.csv")
+file_beer_sales_1 <- read_csv("csv/2-4-1-beer-sales-1.csv")
+file_beer_sales_ab <- read_csv("csv/2-6-1-beer-sales-ab.csv")
 
 
 # 1 共通データの確認 --------------------------------------------------------------------
@@ -52,7 +51,7 @@ data_list <-
 # 動作確認
 # --- 乱数の生成(3章と同じモデル)
 mcmc_result_1 <-
-  stan(file = "book/bayesian_model/stan/2-4-1-calc-mean-variance.stan",
+  stan(file = "stan/2-4/2-4-1-calc-mean-variance.stan",
        data = data_list,
        seed = 1)
 
@@ -70,7 +69,7 @@ mcmc_result_1 %>% print(probs = c(0.025, 0.5, 0.975))
 # 乱数の生成
 # --- 正規分布に従う事前分布を指定
 mcmc_result_2 <-
-  stan(file = "book/bayesian_model/stan/2-6-1-normal-prior.stan",
+  stan(file = "stan/2-6/2-6-1-normal-prior.stan",
        data = data_list,
        seed = 1)
 
@@ -83,7 +82,7 @@ mcmc_result_2 %>% print(probs = c(0.025, 0.5, 0.975))
 # モデル構築
 # --- 乱数の生成(対数密度加算文の使用)
 mcmc_result_3 <-
-  stan(file = "book/bayesian_model/stan/2-6-2-lp.stan",
+  stan(file = "stan/2-6/2-6-2-lp.stan",
        data = data_list,
        seed = 1)
 
@@ -94,7 +93,7 @@ mcmc_result_3 %>% print(probs = c(0.025, 0.5, 0.975))
 # 乱数の生成
 # --- 対数密度加算文の使用、事前分布を正規分布にした
 mcmc_result_4 <-
-  stan(file = "book/bayesian_model/stan/2-6-3-lp-normal-prior.stan",
+  stan(file = "stan/2-6/2-6-3-lp-normal-prior.stan",
        data = data_list,
        seed = 1)
 
@@ -105,7 +104,7 @@ mcmc_result_4 %>% print(probs = c(0.025, 0.5, 0.975))
 # 乱数の生成
 # --- 対数密度加算文の使用、ベクトル化
 mcmc_result_5 <-
-  stan(file = "book/bayesian_model/stan/2-6-4-lp-normal-prior-vec.stan",
+  stan(file = "stan/2-6/2-6-4-lp-normal-prior-vec.stan",
        data = data_list,
        seed = 1)
 
@@ -143,7 +142,7 @@ data_list_ab <-
 # モデル構築
 # --- 乱数の生成
 mcmc_result_6 <-
-  stan(file = "book/bayesian_model/stan/2-6-5-difference-mean.stan",
+  stan(file = "stan/2-6/2-6-5-difference-mean.stan",
        data = data_list_ab,
        seed = 1)
 
