55ce5b421b0cbe7fdfd87200a04684b9ef543b71
Keido Sumi <delta0726@nta.pial.jp>
2022-04-22 08:08:55 +0900

    Update chap3-05

--- book/bayesian_model/3-05_brmsの使い方.R
+++ book/bayesian_model/3-05_brmsの使い方.R
@@ -1,14 +1,14 @@
-#***************************************************************************************
-# Title     : ベイズ統計モデリングによるデータ分析入門
-# Chapter   : 3-5 brmsの使い方
-# Objective : TODO
-# Created by: Owner
-# Created on: 2021/4/11
-# Page      : P185 - P200
-#***************************************************************************************
+# **********************************************************************************
+# Title   : ベイズ統計モデリングによるデータ分析入門
+# Chapter : 3 一般化線形モデル
+# Theme   : 5 brmsの使い方
+# Date    : 2022/4/22
+# Page    : P185 - P200
+# URL     : https://logics-of-blue.com/r-stan-bayesian-model-intro-book-support/
+# **********************************************************************************
 
 
-# ＜テーマ＞
+# ＜概要＞
 # - {brms}ライブラリの使い方を学ぶ
 #   --- Bayesian Regression Models using Stan
 #   --- Stanコードを直接記述する必要がないので実装のハードルが低い
@@ -42,13 +42,18 @@ rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 # 分析対象のデータ
-file_beer_sales_2 <- read_csv("book/bayesian_model/csv/3-2-1-beer-sales-2.csv")
+file_beer_sales_2 <- read_csv("csv/3-2-1-beer-sales-2.csv")
 
 # データ確認
 file_beer_sales_2 %>% print()
 
 
 # 1 単回帰モデルの推定 --------------------------------------------------------------
+
+# ＜ポイント＞
+# - brms::brm()を用いると通常の回帰の記法でstanによるMCMCを使った回帰を行うことができる
+#   --- stanファイルを定義する必要がない
+
 
 # モデル構築
 # --- formula： modelの構造を指定
@@ -60,6 +65,8 @@ simple_lm_brms <-
       seed    = 1)
 
 # 結果確認
+simple_lm_brms %>% class()
+simple_lm_brms %>% names()
 simple_lm_brms %>% print()
 
 # MCMCサンプルの取得
@@ -76,16 +83,17 @@ simple_lm_brms %>% plot()
 
 # 線形予測子
 # --- formulaとfamilyでモデルの構造を指定する
-# --- 複雑なformulaはbf関数で作成（formula関数のようなもの）
+# --- 複雑なformulaはbf関数で作成する（formula関数のようなもの）
 simple_lm_formula <- bf(sales ~ temperature)
+simple_lm_formula %>% class()
 
-# リンク関数
-# --- 正規分布
-# --- 二項分布
-# --- ポアソン分布
-gaussian()
-binomial()
-poisson()
+# 確率分布とリンク関数
+# --- 確率分布は関数名が示す（正規分布, 二項分布, ポアソン分布）
+# --- リンク関数は関数内で定義（identity, logit, log）
+# --- stats::family()の関数群
+gaussian(link = "identity")
+binomial(link = "logit")
+poisson(link = "log")
 
 # モデル構築
 # --- simple_lm_brmsと結果は同じ
@@ -106,17 +114,23 @@ simple_lm_brms_2 %>% print()
 
 # 3 事前分布の変更 --------------------------------------------------------------
 
-# 事前分布の取得
+# ＜ポイント＞
+# - brmsを用いてモデルを推定したときは予め弱情報事前分布が指定される
+
+
+# 弱情報事前分布の取得
 simple_lm_brms %>% prior_summary()
 
-# 事前分布を無情報事前分布にする
+# モデル構築
+# --- prior引数で事前分布を無情報事前分布にする
+# --- prior引数を空欄にすることでstanの標準設定である｢幅広い一様分布｣が使用される
 simple_lm_brms_3 <-
   brm(formula = sales ~ temperature,
       family = gaussian(),
       data = file_beer_sales_2,
       seed = 1,
-      prior = c(set_prior("", class = "Intercept"),
-                set_prior("", class = "sigma")))
+      prior = c(set_prior(prior = "", class = "Intercept"),
+                set_prior(prior = "", class = "sigma")))
 
 # 結果確認
 simple_lm_brms_3 %>% print()
@@ -126,9 +140,8 @@ simple_lm_brms_3 %>% print()
 simple_lm_brms_3 %>% prior_summary()
 
 
-# ＜参考1＞
-# その他の係数の事前分布の変更
-# 気温の係数
+# ＜参考1：＞
+# 事前分布の変更
 set_prior("normal(0,100000)", class = "b", coef = "temperature")
 
 # 事前分布の標準設定の確認
@@ -172,26 +185,37 @@ standata_brms %>% glimpse()
 
 # 6 rstanでbrmsの結果を再現 ---------------------------------------------------------
 
+# ＜ポイント＞
+# - stanファイルを経由してモデルを構築して一致することを確認する
+
+
 # モデル構築
 # --- rstanでbrmsのモデルを実行
 simple_lm_brms_stan <-
-  stan(file = "book/bayesian_model/stan/3-5-1-brms-stan-code.stan",
+  stan(file = "stan/3-5/3-5-1-brms-stan-code.stan",
        data = standata_brms,
        seed = 1)
 
 # 結果確認
 # --- rstanを使ったときの実行結果
+# --- brmsを使ったときの実行結果
 simple_lm_brms_stan %>%
   print(pars = c("b_Intercept", "b[1]", "sigma"),
         probs = c(0.025, 0.5, 0.975))
 
-# brmsを使ったときの実行結果
-simple_lm_brms_3 %>% print()
+simple_lm_brms_3 %>%
+  print(pars = c("b_Intercept", "b[1]", "sigma"),
+        probs = c(0.025, 0.5, 0.975))
 
 
 # 7 事後分布の可視化 ----------------------------------------------------------------
 
-# 係数の95%ベイズ信用区間
+# ＜ポイント＞
+# - stanplot()で事前プロットやトレースプロット以外のプロットも作成することができる
+#   --- パラメータの信頼区間を確認
+
+# プロット作成
+# --- 係数の95%ベイズ信用区間
 simple_lm_brms %>%
   stanplot(type = "intervals",
            pars = "^b_",
@@ -201,14 +225,22 @@ simple_lm_brms %>%
 
 # 8 brmsによる予測 -----------------------------------------------------------------
 
+# ＜ポイント＞
+# - ベイズ統計モデルは結果解釈だけでなく予測も行うことができる
+#   --- fitted()やpredict()を用いる（後ほど計算証明）
+
+
 # データ作成
 # --- 予測のための説明変数
 new_data <- data.frame(temperature = 20)
+new_data %>% print()
 
-# 回帰直線の信用区間付きの予測値
+# 予測
+# --- 回帰直線の信用区間付きの予測値
 simple_lm_brms %>% fitted(new_data)
 
-# 予測区間付きの予測値
+# 予測
+# --- 予測区間付きの予測値
 set.seed(1)
 simple_lm_brms %>% predict(new_data)
 
@@ -224,27 +256,37 @@ mcmc_sample <- simple_lm_brms %>% as.mcmc(combine_chains = TRUE)
 mcmc_sample %>% as_tibble()
 
 # データ格納
-# --- 推定されたパラメタ別に保存しておく
+# --- 推定されたパラメタ別にベクトルで格納
 mcmc_b_Intercept   <- mcmc_sample[,"b_Intercept"]
 mcmc_b_temperature <- mcmc_sample[,"b_temperature"]
 mcmc_sigma         <- mcmc_sample[,"sigma"]
 
 # 予測作成
-# --- 予測された期待値のMCMCサンプルを得る
+# --- MCMCサンプルごとに予測値が得られる
 saigen_fitted <- mcmc_b_Intercept + 20 * mcmc_b_temperature
+saigen_fitted %>% head()
+saigen_fitted %>% length()
 
 
 # ***** 回帰直線の信用区間付きの予測値 ******
+
+# ＜ポイント＞
+# - fitted()のEstimateは予測値の平均値として定義される
+#   --- EstimateやQ2.5 / Q97.5が一致
 
 # fittedの再現
 saigen_fitted %>% mean()
 saigen_fitted %>% quantile(probs = c(0.025, 0.975))
 
-# 確認
+# fitted
 simple_lm_brms %>% fitted(new_data)
 
 
 # ***** 予測区間付きの予測値 ******
+
+# ＜ポイント＞
+# - 予測区間を得る場合にはモデルのsigmaで表現されたデータのバラツキも加味する必要がある
+
 
 # 予測分布のMCMCサンプルを得る
 set.seed(1)
@@ -252,13 +294,17 @@ saigen_predict <-
   rnorm %>%
     do.call(c(4000, list(mean = saigen_fitted, sd = mcmc_sigma)))
 
+# 確認
+saigen_predict %>% head()
+saigen_predict %>% length()
+
 # predictの再現
 saigen_predict %>% mean()
 saigen_predict %>% quantile(probs = c(0.025, 0.975))
 
 # 確認
 set.seed(1)
-simple_lm_brms %>% predict(data.frame(temperature = 20))
+simple_lm_brms %>% predict(new_data)
 
 
 # 10 回帰直線の図示 --------------------------------------------------------
