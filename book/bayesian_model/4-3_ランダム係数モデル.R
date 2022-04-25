eb2b904d8fa965301c4b3e95702bcc1754fa045f
Keido Sumi <delta0726@nta.pial.jp>
2022-04-26 06:59:14 +0900

    Update chap4-3

--- book/bayesian_model/4-3_ランダム係数モデル.R
+++ book/bayesian_model/4-3_ランダム係数モデル.R
@@ -1,21 +1,22 @@
-#***************************************************************************************
-# Title     : ベイズ統計モデリングによるデータ分析入門
-# Chapter   : 4-3 ランダム係数モデル
-# Objective : TODO
-# Created by: Owner
-# Created on: 2021/4/11
-# Page      : P260 - P266
-#***************************************************************************************
+# **********************************************************************************
+# Title   : ベイズ統計モデリングによるデータ分析入門
+# Chapter : 4 一般化線形混合モデル
+# Theme   : 3 ランダム係数モデル
+# Date    : 2022/4/26
+# Page    : P260 - P266
+# URL     : https://logics-of-blue.com/r-stan-bayesian-model-intro-book-support/
+# **********************************************************************************
 
 
-# ＜テーマ＞
-# -
+# ＜概要＞
+# - ランダム係数モデルはランダム効果が加わることで他の説明変数の固定効果の強さが増減する
 
 
 # ＜目次＞
 # 0 準備
-
-
+# 1 データ確認
+# 2 交互作用を用いた一般化線モデル
+# 3 ランダム係数モデル
 
 
 # 0 準備 -----------------------------------------------------------------------
@@ -30,20 +31,36 @@ library(brms)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-
 # データロード
-fish_num_climate_4 <- read.csv("book/bayesian_model/csv/4-3-1-fish-num-4.csv")
+fish_num_climate_4 <- read.csv("csv/4-3-1-fish-num-4.csv")
+
+
+# 1 データ確認 ----------------------------------------------------------------------
+
+# ＜ポイント＞
+# - グループ情報としてhumanが追加されている
+#   --- 同一人物が別の時間に釣りをしたという想定する
+#   --- サンプル数が94しかない点に注意（Jさんのみ4回）
+
+
 # データ確認
-fish_num_climate_4 %>% print()
+fish_num_climate_4 %>% as_tibble()
 fish_num_climate_4 %>% map(table)
 
 # データ要約
 fish_num_climate_4 %>% summary()
 
 
-# 1 交互作用を用いたモデル化 -------------------------------------------------------
+# 2 交互作用を用いた一般化線モデル -----------------------------------------------------
 
-# 交互作用を組み込んだポアソン回帰モデル
+# ＜ポイント＞
+# - ポアソン回帰モデルに交互作用を用いることで適切なモデリングが可能
+#   --- humanをダミー変数として固定効果で処理する（ランダム効果は使用しない）
+#   --- Jさん(10)のみ回帰曲線の方向が逆になっている点に注意
+
+
+# モデル構築
+# --- 交互作用を組み込んだポアソン回帰モデル
 glm_pois_brms_interaction <-
   brm(formula = fish_num ~ temperature * human,
       family = poisson(),
@@ -58,21 +75,30 @@ glm_pois_brms_interaction %>% print()
 glm_pois_brms_interaction %>% stanplot(type = "rhat")
 
 
-# 回帰曲線を描く
-# データの分割
-conditions <- data.frame(
-  human = c("A","B","C","D","E","F","G","H","I","J"))
+# データ作成
+# --- プロット用
+conditions <- tibble(human = c("A","B","C","D","E","F","G","H","I","J"))
 
-# 図示
+# プロット作成
+# --- Jさん(10)のみ回帰曲線の方向が逆になっている
+# --- 気温が低いときにたまたまたくさん釣れたため（ダミー変数により個人の効果は適切に表現している）
 glm_pois_brms_interaction %>%
-  marginal_effects(effects = "temperature",
-                   conditions = conditions) %>%
+  conditional_effects(effects = "temperature",
+                      conditions = conditions) %>%
   plot(points = TRUE)
 
 
-# 2 brmsによるランダム係数モデルの推定 --------------------------------------------------------
+# 3 ランダム係数モデル ---------------------------------------------------------------
 
-# ランダム係数モデル
+# ＜ポイント＞
+# - ランダム効果を用いて上記のモデリングを行う
+#   --- 固定効果はtemeratureのみなので全体として似たような傾向を示すようになる
+#   --- 全体から説得力を借用している（縮約）
+#   --- 分析対象により縮約を利用するのが適切でないケースもある（固定効果として扱うべきもの）
+
+
+# モデル構築
+# --- ランダム係数モデル
 glmm_pois_brms_keisu <-
   brm(formula = fish_num ~ temperature + (temperature||human),
       family = poisson(),
@@ -86,26 +112,22 @@ glmm_pois_brms_keisu <-
 # 推定結果
 glmm_pois_brms_keisu %>% print()
 
-# 参考：トレースプロットなど
+# 確認
+# --- トレースプロットなど
+# --- 弱情報事前分布
+# --- 収束の確認
 glmm_pois_brms_keisu %>% plot()
-
-# 参考：弱情報事前分布
 glmm_pois_brms_keisu %>% prior_summary()
-
-# 参考：収束の確認
 glmm_pois_brms_keisu %>% stanplot(type = "rhat")
 
-# 回帰曲線を描く
-# データの分割
-conditions <- data.frame(
-  human = c("A","B","C","D","E","F","G","H","I","J"))
+# データ作成
+# --- プロット用
+conditions <- tibble(human = c("A","B","C","D","E","F","G","H","I","J"))
 
-# 図示
+# プロット作成
+# --- Jさん(10)も他と同じ傾向を示すようになった
 glmm_pois_brms_keisu %>%
   marginal_effects(re_formula = NULL,
                    effects = "temperature",
                    conditions = conditions) %>%
   plot(points = TRUE)
-
-
-
