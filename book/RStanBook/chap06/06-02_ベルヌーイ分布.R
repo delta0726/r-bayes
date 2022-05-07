f3d19f5053e0d923f1ac18a10213bd2cac2b17f9
Keido Sumi <delta0726@nta.pial.jp>
2022-05-08 07:47:29 +0900

    Add chap06-02

--- book/RStanBook/chap06/06-02_ベルヌーイ分布.R
+++ book/RStanBook/chap06/06-02_ベルヌーイ分布.R
@@ -33,7 +33,7 @@ d %>% print()
 
 d %>%
   ggplot(aes(x = X, y = Y)) +
-    geom_bar(stat = 'identity') +
-    labs(x = 'y', y = 'probability') +
-    scale_x_continuous(breaks = seq(0, 1, 1)) +
-    theme_bw(base_size = 18)
+  geom_bar(stat = 'identity') +
+  labs(x = 'y', y = 'probability') +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  theme_bw(base_size = 18)
